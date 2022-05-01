cf72bb8d7b6ae2840c21b6c68d8c3a3039238e39
latortuered <wriziw@gmail.com>
2022-05-01 06:30:40 +0200

    modif bannir_traitement

--- bannir_traitement.php
+++ bannir_traitement.php
@@ -1,6 +1,8 @@
 <?php
 	require_once 'config.php';
     $id = $_POST['idMembre'];
+    $supprime = $connexion->prepare("DELETE FROM Articles  where idVendeur = $id ");
+    $supprime->execute();
     $supprime = $connexion->prepare("DELETE FROM Membres  where idMembre = $id ");
     $supprime->execute();
     header('Location:membres.php');
